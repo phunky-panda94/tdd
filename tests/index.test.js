c3f5e63d4c7bde9bba09e12a8c3c4968634886d4
phunky-panda94 <victor.nim94@outlook.com>
2021-10-12 13:49:13 +1100

    initial commit

---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+	
+const index = require('../src/index');
+
+describe('example test suite', () => {
+
+	test('example test', () => {
+
+		expect(exampleFunction()).toBe());
+
+	});
+
+});
